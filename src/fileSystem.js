061464ef953e22122bc83e041be4b9a591a58456
cristobalhiza <cristobalhiza@gmail.com>
2024-08-07 20:54:38 -0400

    agrega objeto manager de archivos

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -2,29 +2,29 @@ const fs = require("fs");
 
 let rutaArchivo = "./src/archivos/archivoSync.txt";
 let texto1 =
-  " SYNCEsse elit esse sunt culpa labore pariatur occaecat. Mollit exercitation consequat ad nisi ea in. In irure do ad labore ea esse labore incididunt Lorem nostrud est nostrud elit sit. Incididunt est dolor nisi Lorem anim qui laboris laborum enim irure do. Commodo fugiat anim consequat cillum elit qui in minim nostrud velit irure.";
+    " SYNCEsse elit esse sunt culpa labore pariatur occaecat. Mollit exercitation consequat ad nisi ea in. In irure do ad labore ea esse labore incididunt Lorem nostrud est nostrud elit sit. Incididunt est dolor nisi Lorem anim qui laboris laborum enim irure do. Commodo fugiat anim consequat cillum elit qui in minim nostrud velit irure.";
 let texto2 =
-  "ASYNC Aliquip nostrud minim ipsum proident laborum incididunt voluptate nostrud enim laborum veniam magna ullamco ipsum. Ea ipsum tempor esse mollit nisi cupidatat nulla nulla amet. Proident aute enim officia culpa duis duis. In sunt veniam laboris do aliquip qui aliqua dolor nulla in laborum deserunt magna velit. Culpa ea officia duis sint est laborum enim reprehenderit ea ex duis eu.";
-fs.writeFileSync(
-  rutaArchivo,
-  texto1,
-  { encoding: "utf-8" } /*Ese es el encoding por defecto si no se coloca*/
-);
+    "ASYNC Aliquip nostrud minim ipsum proident laborum incididunt voluptate nostrud enim laborum veniam magna ullamco ipsum. Ea ipsum tempor esse mollit nisi cupidatat nulla nulla amet. Proident aute enim officia culpa duis duis. In sunt veniam laboris do aliquip qui aliqua dolor nulla in laborum deserunt magna velit. Culpa ea officia duis sint est laborum enim reprehenderit ea ex duis eu.";
+// fs.writeFileSync(
+//   rutaArchivo,
+//   texto1,
+//   { encoding: "utf-8" } /*Ese es el encoding por defecto si no se coloca*/
+// );
 
-let datosArchivo = fs.readFileSync(
-  rutaArchivo,
-  { encoding: "utf-8" } /*en este caso no se puede omitir para leer*/
-);
+// let datosArchivo = fs.readFileSync(
+//   rutaArchivo,
+//   { encoding: "utf-8" } /*en este caso no se puede omitir para leer*/
+// );
 
-fs.appendFileSync(rutaArchivo, "\n\n\tTEXTO AGREGADO AL FINAL");
+// fs.appendFileSync(rutaArchivo, "\n\n\tTEXTO AGREGADO AL FINAL");
 
-console.log(datosArchivo);
+// console.log(datosArchivo);
 
 // setTimeout(() => fs.unlinkSync(rutaArchivo), 2000);
 
-fs.existsSync(rutaArchivo)
-  ? console.log("El archivo existe")
-  : console.log("El archivo no existe");
+// fs.existsSync(rutaArchivo)
+//   ? console.log("El archivo existe")
+//   : console.log("El archivo no existe");
 //Js dirá que existe porque setTimeout es async
 
 //METODOS ASYNC
@@ -76,24 +76,61 @@ fs.existsSync(rutaArchivo)
 
 //usar encadenamiento de promesas en vez de usar .then después de cada una de las promesas (evitar el hell)
 
-const archivos = async () => {
-  try {
-    await fs.promises.writeFile(rutaArchivo, texto2);
-    let textoRecuperado = await fs.promises.readFile(rutaArchivo, {
-      encoding: "utf-8",
-    });
-    console.log(textoRecuperado);
-    await fs.promises.appendFile(rutaArchivo, "\n\nAGREGA TEXTO");
-    console.log("Texto Agregado");
+// const archivos = async () => {
+//   try {
+//     await fs.promises.writeFile(rutaArchivo, texto2);
+//     let textoRecuperado = await fs.promises.readFile(rutaArchivo, {
+//       encoding: "utf-8",
+//     });
+//     console.log(textoRecuperado);
+//     await fs.promises.appendFile(rutaArchivo, "\n\nAGREGA TEXTO");
+//     console.log("Texto Agregado");
 
-    setTimeout(async () => {
-      await fs.promises.unlink(rutaArchivo);
-      console.log("archivo eliminado");
-    }, 2000);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+//     setTimeout(async () => {
+//       await fs.promises.unlink(rutaArchivo);
+//       console.log("archivo eliminado");
+//     }, 2000);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
 
 // archivos()
 
+class HeroesManager {
+    constructor(ruta) {
+        this.path = ruta
+    }
+
+    async getHeroes() {
+        if (fs.existsSync(this.path)) {
+            return JSON.parse(await fs.promises.readFile(this.path, { encoding: "utf-8" }))
+        } else {
+            return []
+        }
+    }
+
+    async addHeroe(name) {
+        //validaciones
+        let heroes = await this.getHeroes()
+        let id = 1
+        if (heroes.length > 0) {
+            id = Math.max(...heroes.map(d => d.id)) + 1
+        }
+        heroes.push({
+            id, name
+        })
+        await fs.promises.writeFile(this.path, JSON.stringify(heroes, null, 2))
+        console.log(`Heroe ${name} generado con id ${id}`)
+    }
+}
+
+const heroesManager = new HeroesManager("./src/archivos/heroes.json")
+// heroesManager.getHeroes().then(heroes=>console.log(heroes))
+
+const app = async () => {
+    console.log(await heroesManager.getHeroes())
+    await heroesManager.addHeroe("Robin")
+}
+
+app()
